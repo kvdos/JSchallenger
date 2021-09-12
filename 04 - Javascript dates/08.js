fd145d97862071805a693e0e9a35f14475b6ec18
Alejandro <alepeddia@gmail.com>
2021-09-12 21:51:51 +0200

    Update wording 08 dates

--- 04 - Javascript dates/08.js
+++ 04 - Javascript dates/08.js
@@ -1,3 +1,9 @@
+// Return the next nearest quarter hour of a date
+
+// Write a function that takes a Date instance as argument
+// It should return the next nearest quarter hour in minutes
+// For example, if the given date has the time 10:01 the function should return 15
+
 function myFunction(date) {
   const min = date.getMinutes();
   for (let i = 15; i <= 60; i += 15) {
